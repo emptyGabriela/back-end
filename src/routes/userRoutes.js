0e81db30c5ddebc80e64f6e704a72b94a27c36ad
emptyGabriela <00141621@uca.edu/sv>
2023-12-04 20:15:55 -0600

    dd

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -63,7 +63,7 @@ router.get("/usuarios/:id", verificarToken, async (req, res) => {
 
 // Actualizar usuario (PUT)
 router.put("/usuarios/:id", verificarToken, async (req, res) => {
-//   try {
+  try {
     var { id } = req.params;
     console.log(req.body)
     var {
@@ -90,14 +90,14 @@ router.put("/usuarios/:id", verificarToken, async (req, res) => {
       username
     };
 
-    if (content.password) {
+    if (password) {
       updatedUser.password = password;
     }
 
     User.update({ _id: id }, updateUser);
-//   } catch (error) {
+  } catch (error) {
     res.status(500).json({ mensaje: "Error en el servidor.", error: error });
-//   }
+  }
 });
 
 // Eliminar usuario (DELETE)
