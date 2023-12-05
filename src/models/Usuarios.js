342dc71963e08479302d34f5a948dbfa6c853019
emptyGabriela <00141621@uca.edu/sv>
2023-12-04 20:31:51 -0600

    xx

--- src/models/Usuarios.js
+++ src/models/Usuarios.js
@@ -56,6 +56,13 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 8);
 });
 
+userSchema.pre("update", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 8);
+});
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
